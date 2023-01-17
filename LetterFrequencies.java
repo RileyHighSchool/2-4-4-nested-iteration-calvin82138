5471ef13bb959ae0d2217f43de7c5e7fd04baa0c
calvin82138 <calvin82138@students.sbcsc.k12.in.us>
2023-01-17 12:34:41 -0500

    finished assignment

--- LetterFrequencies.java
+++ LetterFrequencies.java
@@ -9,7 +9,27 @@ public class LetterFrequencies
 		String phrase = "This is a phrase!";
 
 		System.out.println("The following shows the letter frequencies for the phrase");
+		
 
     /* your code here */
+	
+		for(int i = 0; i < 26; i++)
+		{
+			String letter = letters.substring(i,i+1);
+			int counter = 0;
+
+			for(int j = 0; j < phrase.length(); j++){
+				
+				String speech = phrase.substring(j,j+1);
+
+				if(letter.equals(speech)){
+					counter +=1;
+				}
+			}
+
+			System.out.println("There are " + counter +  " " + letter );
+
+		}
 	}
-}
+	
+	}
